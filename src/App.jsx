9aaed2b202274f48bdd7de188f83afe81ce3fb57
RebeccaCalixte <rebecca.calixte@sparksoftcorp.com>
2025-01-10 10:15:28 -0500

    Updated React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+ return (
+<div className="App">
+<header className="App-header">
+<h1>Welcome to Unicorn Land! 🦄✨</h1>
+<p>Let's build something magical! 🌟</p>
+</header>
+</div>
+ );
+}
+export default App;
